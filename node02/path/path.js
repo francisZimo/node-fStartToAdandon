67694dad2371c7f441f37fa82d427d47cbc582f2
wyj <2443573565@qq.com>
2019-03-04 15:57:42 +0800

    node02

--- node02/path/path.js
+++ node02/path/path.js
@@ -1,11 +1,13 @@
 let path=require('path');
 //拼接一个路径出来
-console.log(path.join(__dirname,'./b/c','..'))
+// console.log(path.join(__dirname,'./b/c'))
+// console.log(__dirname);
 //解析一个绝对路径出来
-console.log(path.resolve('./a','/b'))
+console.log(path.resolve(__dirname,'./a','../../b'))
 //看一下系统的分隔符
-console.log(path.delimiter);  //环境变量分隔符
-console.log(path.win32.delimiter)
-console.log(path.posix.sep);
+// console.log(path.delimiter);  //环境变量分隔符
+// console.log(path.win32.delimiter)
+// console.log(path.posix.sep);
+
 
 
